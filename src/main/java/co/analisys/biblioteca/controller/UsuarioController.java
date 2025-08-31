479d36b2e465e60165d9cd24aeb70afacd24f501
Camilo Carmona Valencia <101128930+Camilin1904@users.noreply.github.com>
2025-08-31 18:00:07 -0500

    add things :)

--- src/main/java/co/analisys/biblioteca/controller/UsuarioController.java
+++ src/main/java/co/analisys/biblioteca/controller/UsuarioController.java
@@ -4,6 +4,8 @@ import co.analisys.biblioteca.model.Email;
 import co.analisys.biblioteca.model.Usuario;
 import co.analisys.biblioteca.model.UsuarioId;
 import co.analisys.biblioteca.service.UsuarioService;
+import io.swagger.v3.oas.annotations.Operation;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,11 +15,19 @@ public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
+    @Operation(
+        summary = "Obtener usuario por ID",
+        description = "Obtiene los detalles de un usuario dado su ID único."
+    )
     @GetMapping("/{id}")
     public Usuario obtenerUsuario(@PathVariable String id) {
         return usuarioService.obtenerUsuario(new UsuarioId(id));
     }
 
+    @Operation(
+        summary = "Cambiar email de usuario",
+        description = "Actualiza la dirección de correo electrónico de un usuario específico."
+    )
     @PutMapping("/{id}/email")
     public void cambiarEmail(@PathVariable String id, @RequestBody String nuevoEmail) {
         usuarioService.cambiarEmailUsuario(new UsuarioId(id), new Email(nuevoEmail));
